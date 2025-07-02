e1385b2ecbf8e6487556cda212d602025cd8d074
Hannes Vieth <hannesvieth@gmail.com>
2025-07-02 19:47:05 +0300

    Tehtävät 6.20-6.22 tehty - React Query otettu käyttöön

---
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,35 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
+
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    } })
+  
+  const onCreate = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    if (content.length < 5) {
+      return
+    }
+    event.target.anecdote.value = ''
+    console.log('new anecdote')
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+}
+
+  return (
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name='anecdote' />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
